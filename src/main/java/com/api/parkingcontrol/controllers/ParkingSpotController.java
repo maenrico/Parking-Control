0c3faa636752082f7104cf72634eb55e9eecc692
maenrico <55825477+maenrico@users.noreply.github.com>
2022-02-17 14:10:53 -0300

    Criando a Estrutura

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,11 @@
+package com.api.parkingcontrol.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+
+}
